4eba17749168dfc57ea69097717abe59bfabd183
YurySE <[chuksin.yury@gmail.com]>
2024-07-26 17:11:47 +0300

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'Главная страница магазина - HOME',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'is_authenticated': True
+    }
+
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('About page')
